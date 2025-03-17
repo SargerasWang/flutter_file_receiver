012b0df1020749f4926675b5cfd195871826748b
sargeras <sargeras@bjxrwt.com>
2025-03-17 22:46:02 +0800

    更新版本

--- android/src/main/kotlin/com/sargeraswang/flutter_file_receiver/FlutterFileReceiverPlugin.kt
+++ android/src/main/kotlin/com/sargeraswang/flutter_file_receiver/FlutterFileReceiverPlugin.kt
@@ -1,6 +1,5 @@
 package com.sargeraswang.flutter_file_receiver
 
-import androidx.annotation.NonNull
 import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
@@ -19,7 +18,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 
 /** FlutterFileReceiverPlugin */
